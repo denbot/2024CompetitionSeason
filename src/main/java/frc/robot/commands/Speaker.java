c655ae36db1023b4be5438635a3207cb23a5a345
Reuben Kaskubar <reubenkaskubar@angelbotics.org>
2024-02-01 07:06:39 -0700

    Shooter not finished, need to complete pivot motor and keybindings

---
+++ src/main/java/frc/robot/commands/Speaker.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter;
+
+public class Speaker extends Command {
+  
+  private final Shooter shooter;
+  private double speed = 0.5;
+
+  public Speaker(Shooter shooter) {
+    this.shooter = shooter;
+    addRequirements(shooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    speed = MathUtil.clamp(speed, 0, 1);
+    shooter.startMotors(speed, "Speaker");
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}

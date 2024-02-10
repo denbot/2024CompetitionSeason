7a57d0a903516ec0acb725e1a9f1768d14aae118
Reuben Kaskubar <reubenkaskubar@angelbotics.org>
2024-02-10 13:40:06 -0700

    Fixed build failing

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -9,10 +9,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public final class Autos {
-  /** Example static factory for an autonomous command. */
-  public static Command exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
-  }
+  /** Reference a command sequence here */
 
   private Autos() {
     throw new UnsupportedOperationException("This is a utility class!");

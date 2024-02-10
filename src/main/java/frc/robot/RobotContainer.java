496f6e8f32523fafb15e6fe134e8cbe7e358e038
Reuben Kaskubar <reubenkaskubar@angelbotics.org>
2024-02-10 13:40:06 -0700

    Fixed build failing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,9 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.Amp;
-import frc.robot.commands.Speaker;
-import frc.robot.commands.Shoot;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.AmpCommand;
+import frc.robot.commands.SpeakerCommand;
+import frc.robot.commands.ShootCommand;
 import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -24,11 +22,10 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final Shooter shooterSubsystem = new Shooter();
-  private final Amp ampCommand = new Amp(shooterSubsystem);
-  private final Speaker speakerCommand = new Speaker(shooterSubsystem);
-  private final Shoot shootCommand = new Shoot(shooterSubsystem);
+  private final AmpCommand ampCommand = new AmpCommand(shooterSubsystem);
+  private final SpeakerCommand speakerCommand = new SpeakerCommand(shooterSubsystem);
+  private final ShootCommand shootCommand = new ShootCommand(shooterSubsystem);
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -51,8 +48,6 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
@@ -66,8 +61,7 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
-  }
+  // public Command getAutonomousCommand() {
+  //   // An example command will be run in autonomous
+  // }
 }

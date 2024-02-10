9e9e8b5753e6123e4b28699fcc7b25d9d4ff2a04
colerog <cole26525@gmail.com>
2024-02-10 10:44:48 -0700

    Revised the entirety of the subsytem code.

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -5,76 +5,75 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-// import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
+import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
  
 public class Intake extends SubsystemBase{
-    private final DigitalInput input0 = new DigitalInput(0);
-    private final DigitalInput input1 = new DigitalInput(1);
-    private final DigitalInput input2 = new DigitalInput(2);
+    private final DigitalInput preIntakeSensor = new DigitalInput(0);
+    private final DigitalInput intakeSensor = new DigitalInput(1);
+    private final DigitalInput shooterSensor = new DigitalInput(2);
 
     private final TalonFX intakeMotor = new TalonFX(0);
-    /** Creates a wait function that can be called on using wait() */
-    public static void wait(int ms) {
-        try
-        {
-            //Uses milliseconds as sleep time
-            Thread.sleep(ms);
-        }
-        catch(InterruptedException ex){
-            Thread.currentThread().interrupt();
+
+    boolean status = intakeMotor.isAlive();
+    //Will change intakeMotorVelocity
+    double intakeMotorVelocity = 1;
+    private VelocityVoltage velocity = new VelocityVoltage(intakeMotorVelocity);
+
+    enum IntakeState {
+        IDLE,  // No motors are moving, no note is inside the mechanism
+        INTAKING, // Motors are moving, note is not yet where we need it to be
+        HOLDING, // No motors are moving, note is where it needs to be and is contained in the robot
+        EJECTING;  // Motors are moving, note is being moved into the gears
+    }
+    public static IntakeState CurrentState = IntakeState.IDLE;
+    
+    public Intake(){
+        // SmartDashboard displays to test sensors
+        SmartDashboard.putBoolean("Motor Functional?", status);
+        SmartDashboard.putNumber("Intake Motor Speed (RPM)", intakeMotorVelocity);
+    }
+
+    @Override
+    public void periodic(){
+        switch (CurrentState) {
+            case IDLE:
+                if (preIntakeSensor.get() == false){ 
+                    intakeMotor.setControl(velocity.withVelocity(intakeMotorVelocity));
+                    CurrentState = IntakeState.INTAKING;
+                }
+                    break;
+                    case INTAKING:
+                    if (preIntakeSensor.get() == true && intakeSensor.get() == true){
+                        CurrentState = IntakeState.IDLE;
+                    }
+                    if (shooterSensor.get() == false){
+                        CurrentState = IntakeState.HOLDING;
+                }
+                    break;
+            case HOLDING:
+                intakeMotor.set(0);
+                    break;
+            case EJECTING:
+                while(shooterSensor.get() == false){
+                    intakeMotor.setControl(velocity.withVelocity(intakeMotorVelocity));
+                }
+                intakeMotor.set(0);
+                CurrentState = IntakeState.IDLE;
+                    break;
         }
     }
-    boolean inside = false;
-    boolean readyToShoot = false;
-    boolean failSafe = false;
-    boolean status = intakeMotor.isAlive();
-    double intakeMotorSpeed = 0;
-    /** Creates a new intake. */
-    public Intake(){
-        //SmartDashboard displays to test sensors
-        SmartDashboard.putBoolean("Motor Functional?", status);
-        SmartDashboard.putBoolean("Engage Intake",input0.get());
-        SmartDashboard.putBoolean("Intaking", input1.get());
-        SmartDashboard.putBoolean("Stored?", input2.get());
-        SmartDashboard.putBoolean("Ready To Fire", readyToShoot);
-        //If there is no note inside and a note is detected we start the intake motors
-        if (input0.get() == true && inside == false){
-            //Starting Intake Motors
-            // intakeMotor.set(0);
-        }
-        if (input1.get() == true && inside == false){
-            //Telling our code that we have a note intaking and we cannot pick up another note
-            inside = true;
-            failSafe = true;
-            
-        }
-        //Failsafe to prevent incorrect sensor detections
-        if (failSafe == true && inside == true){
-            //Time is subject to change
-            wait(5000);
-            if (input2.get() == false){
-                inside = false;
-                failSafe = false;
-            }
-            else return;
-        }
-        if (input2.get() == true && readyToShoot == false){
-            //Detects note inside intake and stops intake motors
-            readyToShoot = true;
-            //intakeMotor.set(0);
-            //wait(0);
-            //intakeMotor.set(0);
-            failSafe = false;
-        } else if(input2.get() == false && readyToShoot == true){
-            //Note has been fired
-            readyToShoot = false;
-            inside = false;
-            //intakeMotor.set(0);
-        }
+    
+    // Called on by shooter subsystem
+    public void shoot(){
+        CurrentState = IntakeState.EJECTING;
+    }
 
+    public void disable(){
+        intakeMotor.set(0);
     }
 }

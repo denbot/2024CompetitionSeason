c65d744df1aede858d398effcf80afa8e660f52d
Elliott Goldberg <elliottgo@gmail.com>
2024-03-05 18:22:15 -0700

    some more changes

--- src/main/java/frc/robot/commands/PrepCommand.java
+++ src/main/java/frc/robot/commands/PrepCommand.java
@@ -7,23 +7,32 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Shooter;
 
-public class AmpCommand extends Command {
-  
+public class PrepCommand extends Command {
+ 
   private final Shooter shooter;
-  private double speed = 1; // TODO: Change to amp motor speed (in rotations per second)
-  private final double ampAlignAngle = 36; // TODO: tune relative to shooting from right in front of amp
+  double angle = 0;
+  double speed = 0;
 
-  public AmpCommand(Shooter shooter) {
+  /** Creates a new PrepCommand. */
+  public PrepCommand(Shooter shooter, double angle, double speed) {
+    // Use addRequirements() here to declare subsystem dependencies.
     this.shooter = shooter;
-    addRequirements(shooter);
   }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    shooter.setAngle(ampAlignAngle);
+    shooter.setAngle(angle);
     shooter.startMotors(speed);
   }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override

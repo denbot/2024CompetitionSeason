3be0c106b296448c26e9dc63b4b764c02ac4f8bd
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2024-03-05 18:00:27 -0700

    add gravity value to arm pid gains

--- src/main/java/frc/robot/generated/ArmTunerConstants.java
+++ src/main/java/frc/robot/generated/ArmTunerConstants.java
@@ -2,6 +2,7 @@ package frc.robot.generated;
 
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.signals.GravityTypeValue;
 
 public class ArmTunerConstants {
 
@@ -12,5 +13,7 @@ public class ArmTunerConstants {
 
   public static final Slot0Configs pivotPIDConfigs = new Slot0Configs()
     .withKP(4.8)
-    .withKD(0.1);
+    .withKD(0.1)
+    .withGravityType(GravityTypeValue.Arm_Cosine)
+    .withKG(0); // tune later so it hovers horizontal (may need to be negative) 0 has to be straight out
 }

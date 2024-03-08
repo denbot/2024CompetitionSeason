af2a33868ea813cea6c056699609620ab5e30cf0
Reuben Kaskubar <reubenkaskubar@angelbotics.org>
2024-03-08 11:53:37 -0700

    Removed time until reset variable; not needed

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -25,7 +25,6 @@ public class Intake extends SubsystemBase {
     private double intakeMotorVelocity = 4;
     private VelocityVoltage velocity = new VelocityVoltage(intakeMotorVelocity);
     private boolean isEjecting = false;
-    private double timeUntilReset = 0.2;
 
     private enum IntakeState {
         IDLE, // No motors are moving, no note is inside the mechanism
@@ -53,7 +52,6 @@ public class Intake extends SubsystemBase {
         if (reverseIntaking == false) {
             isEjecting = true;
         }
-        timeUntilReset = 2;
         switch (currentState) {
             case HOLDING:
                 currentState = IntakeState.IDLE;
@@ -110,13 +108,12 @@ public class Intake extends SubsystemBase {
                     currentState = IntakeState.HOLDING;
                 }
 
-                if (timer.hasElapsed(timeUntilReset)) {
+                if (timer.hasElapsed(0.2)) {
                     currentState = IntakeState.IDLE;
                     timer.stop();
                     timer.reset();
                     intakeMotor.set(0);
                     isEjecting = false;
-                    timeUntilReset = 0.2;
                 }
                 // if (timer.get() == 0 && noteAtPreIntakeSensor){
                 //     timer.start();

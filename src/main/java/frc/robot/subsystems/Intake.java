59ea5e5a2120947cd046033bdfc30d8213a596be
Elliott Goldberg <elliottgo@gmail.com>
2024-03-11 12:41:47 -0600

    Intake clean up: second timer not needed

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -23,6 +23,8 @@ public class Intake extends SubsystemBase {
     private final Servo RightIntakeServo = new Servo(1); // PWM channel is likely subject to change.
     private final Servo LeftIntakeServo = new Servo(2); // PWM channel is likely subject to change.
 
+    private boolean notePassedShooterSensor = false;
+
     private boolean status = intakeMotor.isAlive();
 
     private enum IntakeState {
@@ -33,7 +35,6 @@ public class Intake extends SubsystemBase {
     }
 
     private Timer timer = new Timer();
-    private Timer postIntakeTimer = new Timer();
 
     private static IntakeState currentState = IntakeState.IDLE;
 
@@ -41,8 +42,6 @@ public class Intake extends SubsystemBase {
         intakeMotor.setInverted(true);
         timer.stop();
         timer.reset();
-        postIntakeTimer.stop();
-        postIntakeTimer.reset();
     }
 
     public void optomizeCan() {
@@ -65,62 +64,52 @@ public class Intake extends SubsystemBase {
         SmartDashboard.putBoolean("Shooter Sensor", shooterSensor.get());
         SmartDashboard.putString("Intake State", currentState.name());
         SmartDashboard.putNumber("Timer", timer.get());
-        SmartDashboard.putNumber("Post Intake Timer", postIntakeTimer.get());
         boolean noteAtPreIntakeSensor = ! preIntakeSensor.get();
         boolean noteAtIntakeSensor = ! intakeSensor.get();
         boolean noteAtShooterSensor = ! shooterSensor.get();
         switch (currentState) {
             case IDLE:
 
-                if (intakeMotor.get() != 0) {
+                if (intakeMotor.get() != 0) { // If the intake motor is moving, stop it
                     intakeMotor.set(0);
                 }
 
-                if (noteAtPreIntakeSensor) {
+                if (noteAtPreIntakeSensor) { // If there is a note at the intake, start intaking and make sure that the timers are reset and stopped
                     LeftIntakeServo.setAngle(160);
                     RightIntakeServo.setAngle(0);
 
                     currentState = IntakeState.INTAKING;
+                    notePassedShooterSensor = false;
                     timer.stop();
                     timer.reset();
-                    postIntakeTimer.stop();
-                    postIntakeTimer.reset();
                 }
 
                 break;
             case INTAKING:
 
-                if (noteAtPreIntakeSensor || noteAtIntakeSensor || noteAtShooterSensor) { // If there is a note in the intake subsystem, keep the timer from starting and make sure the motor is moving
-                    if (intakeMotor.get() == 0) {
+                if (noteAtPreIntakeSensor || noteAtIntakeSensor || noteAtShooterSensor) { // If there is a note in the intake subsystem, make sure the motor is moving
+                    if (intakeMotor.get() == 0) { // If the motor is not moving, make it move
                         intakeMotor.set(0.2);
                     }
-                    timer.stop();
-                    timer.reset();
-                } else if (timer.get() == 0 && postIntakeTimer.get() != 0) { // If there is not a note in the subsystem, the timer has not started, and the note has not already passed the shooter sensor, start the timer to disable the motor
-                    timer.start();
+                } else { // The note is not touching any of the sensors
+                    if (! notePassedShooterSensor) { // If the note has gone not past the last sensor
+                        currentState = IntakeState.IDLE;
+                        intakeMotor.stopMotor();
+                    }
                 }
 
-                if (noteAtShooterSensor) { // Start the second timer when the note hits the shooter sensor
-                    postIntakeTimer.reset();
-                    postIntakeTimer.start();
+                if (noteAtShooterSensor) { // Now we know when the note hits the shooter sensor
+                    if (! notePassedShooterSensor) {
+                        notePassedShooterSensor = true;
+                        timer.start();
+                    }
                 }
 
-                if (postIntakeTimer.hasElapsed(0.1)) { // If the second timer has reached 0.1 seconds, we are now holding the note
-                    currentState = IntakeState.HOLDING;
-                    intakeMotor.stopMotor();
-                    timer.stop();
-                    timer.reset();
-                    postIntakeTimer.stop();
-                    postIntakeTimer.reset();
-                }
-                
-                if (timer.hasElapsed(0.2)) { // If there has been no note detected inside the intake subsystem for 0.2 seconds, set back to idle and stop the motors
+                if (timer.hasElapsed(0.1)) { // TODO: tune this value. This timer starts when the note hits the last sensor, and this value stops the intaking process when the timer reaches this value
                     currentState = IntakeState.IDLE;
                     intakeMotor.stopMotor();
                     timer.stop();
                     timer.reset();
-                    postIntakeTimer.stop();
-                    postIntakeTimer.reset();
                 }
 
                 break;
@@ -153,7 +142,5 @@ public class Intake extends SubsystemBase {
         intakeMotor.stopMotor();
         timer.stop();
         timer.reset();
-        postIntakeTimer.stop();
-        postIntakeTimer.reset();
     }
 }

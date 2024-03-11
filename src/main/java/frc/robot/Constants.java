a50b647fe50ca594ade2f4f57e0838021b69869f
Justin Nesselrotte <admin@jnesselr.org>
2024-03-11 17:38:11 -0600

    Initial start on swerve auto / path planner

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,8 +17,21 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-    public static final String canivoreSerial = "D75CCE723353385320202034111303FF";
-  }
+    public static class OperatorConstants {
+        public static final int kDriverControllerPort = 0;
+        public static final String canivoreSerial = "D75CCE723353385320202034111303FF";
+    }
+
+    public static final class AutoConstants {
+        public static final HolonomicPathFollowerConfig autoConfig = new HolonomicPathFollowerConfig(
+                // HolonomicPathFollowerConfig, this should likely live in your Constants class
+                new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+                6.21, // Max module speed, in m/s
+                0.40451045104, // Drive base radius in meters. Distance from robot center to furthest
+                // module.
+                new ReplanningConfig() // Default path replanning config. See the API for the options
+                // here
+        );
+    }
 }

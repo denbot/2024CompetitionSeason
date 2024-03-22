940c9b58792fd383f1536ba217a8fe45381181df
Justin Nesselrotte <admin@jnesselr.org>
2024-03-22 08:40:53 -0600

    Moved sensors to public methods on intake

--- src/main/java/frc/robot/commands/intake/MoveNoteToHoldingCommand.java
+++ src/main/java/frc/robot/commands/intake/MoveNoteToHoldingCommand.java
@@ -3,24 +3,18 @@ package frc.robot.commands.intake;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
 
-import java.util.function.Supplier;
-
 public class MoveNoteToHoldingCommand extends Command {
     private final Intake intake;
-    private final Supplier<Boolean> noteAtShooterSensor;
 
     public MoveNoteToHoldingCommand(
-            Intake intake,
-            Supplier<Boolean> noteAtShooterSensor) {
+            Intake intake) {
         this.intake = intake;
         addRequirements(intake);
-
-        this.noteAtShooterSensor = noteAtShooterSensor;
     }
 
     @Override
     public void execute() {
-        boolean noteAtShooterSensor = this.noteAtShooterSensor.get();
+        boolean noteAtShooterSensor = intake.noteAtShooterSensor();
 
         if(! noteAtShooterSensor) {  // We've moved the note past this sensor
             intake.commands.moveNoteBackToShooterReady.schedule();
--- src/main/java/frc/robot/commands/intake/WaitForIntakeCommand.java
+++ src/main/java/frc/robot/commands/intake/WaitForIntakeCommand.java
@@ -4,24 +4,16 @@ import com.ctre.phoenix6.controls.NeutralOut;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Intake;
 
-import java.util.function.Supplier;
 
 public class WaitForIntakeCommand extends Command {
     private final Intake intake;
-    private final Supplier<Boolean> noteAtPreIntakeSensor;
-    private final Supplier<Boolean> noteAtIntakeSensor;
 
     private final NeutralOut brake = new NeutralOut();
 
     public WaitForIntakeCommand(
-            Intake intake,
-            Supplier<Boolean> noteAtPreIntakeSensor,
-            Supplier<Boolean> noteAtIntakeSensor) {
+            Intake intake) {
         this.intake = intake;
         addRequirements(intake);
-
-        this.noteAtPreIntakeSensor = noteAtPreIntakeSensor;
-        this.noteAtIntakeSensor = noteAtIntakeSensor;
     }
 
     @Override
@@ -31,8 +23,8 @@ public class WaitForIntakeCommand extends Command {
 
     @Override
     public void execute() {
-        boolean noteAtPreIntakeSensor = this.noteAtPreIntakeSensor.get();
-        boolean noteAtIntakeSensor = this.noteAtIntakeSensor.get();
+        boolean noteAtPreIntakeSensor = intake.noteAtPreIntakeSensor();
+        boolean noteAtIntakeSensor = intake.noteAtIntakeSensor();
 
         // If there is a note at the intake, start intaking
         if (noteAtPreIntakeSensor || noteAtIntakeSensor) {

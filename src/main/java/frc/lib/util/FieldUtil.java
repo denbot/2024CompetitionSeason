504ef241026d4cbe9c5871ef6075b0c497f79bb4
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2024-03-30 07:56:13 -0600

    update field markings for speakers

--- src/main/java/frc/lib/util/FieldUtil.java
+++ src/main/java/frc/lib/util/FieldUtil.java
@@ -9,10 +9,19 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class FieldUtil {
+    // dimensions: https://firstfrc.blob.core.windows.net/frc2024/FieldAssets/2024FieldDrawings.pdf
+
+    // page 239
     public static final Translation2d BlueSpeakerPosition = new Translation2d(8.27 - 8.308467, 4.105 + 1.442593);
     public static final Translation2d RedSpeakerPosition = new Translation2d(8.27 + 8.308467, 4.105 + 1.442593);
 
-    public static final double SpeakerHeight = Units.inchesToMeters(79.0);
+    // page 120
+    public static final double SpeakerHeight = Units.inchesToMeters(78.13);
+    public static final double SpeakerCoverHeight = Units.inchesToMeters(82.90);
+
+    public static final double SpeakerOpeningHeight = SpeakerCoverHeight - SpeakerHeight;
+    public static final double SpeakerOpeningLength = Units.inchesToMeters(18.11);
+    public static final double SpeakerOpeningWidth = Units.inchesToMeters(41.38);
 
 
     public static boolean isAllianceBlue() {
@@ -24,6 +33,7 @@ public class FieldUtil {
 
         return isAllianceBlue;
     }
+
     /**
      * Get the position of whichever alliance you are on
      *
@@ -41,15 +51,16 @@ public class FieldUtil {
     }
 
     public static Translation3d getCenterOfAllianceSpeakerOpening() {
-        double len = Units.inchesToMeters(18.0);
-        double height = SpeakerHeight + Math.tan(Math.toRadians(14)) * len;
-
         if (isAllianceBlue()) {
-            return new Translation3d(getAllianceSpeakerPosition().getX() + len / 2.0, getAllianceSpeakerPosition().getY(),
-                    height);
+            return new Translation3d(
+                    getAllianceSpeakerPosition().getX() + SpeakerOpeningLength / 2.0,
+                    getAllianceSpeakerPosition().getY(),
+                    SpeakerOpeningHeight);
         }
 
-        return new Translation3d(getAllianceSpeakerPosition().getX() - len / 2.0, getAllianceSpeakerPosition().getY(),
-                height);
+        return new Translation3d(
+                getAllianceSpeakerPosition().getX() - SpeakerOpeningLength / 2.0,
+                getAllianceSpeakerPosition().getY(),
+                SpeakerOpeningHeight);
     }
 }
